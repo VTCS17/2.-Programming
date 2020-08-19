3843eed9d01592c37886955ef6a3fdaa1ca164d1
VTCS17 <69059602+VTCS17@users.noreply.github.com>
2020-08-19 21:43:05 +0100

    Update Fifth Program.py

--- Python/Fifth Program.py
+++ Python/Fifth Program.py
@@ -1,1 +1,25 @@
 print("Working with numbers")
+
+print(2)
+print(-2.0987)
+print(2 + 2.09)
+print(3 * 4 + 5)
+print(3 * (4 + 5))
+print(10 % 3)
+my_num = 5
+print(my_num)
+print(str(my_num))
+print(str(my_num) + " my fav number")
+my_num = -5
+print(abs(my_num))
+print(pow(my_num,3))
+print(max(4,9))
+print(min(4,9))
+print(round(3.2))
+print(round(3.7))
+
+from math import *
+
+print(floor(3.7))
+print(ceil(3.7))
+print(sqrt(36))
