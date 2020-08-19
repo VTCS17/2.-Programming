ff1735cab587bff915837d59b3a29081cb5ccff3
VTCS17 <69059602+VTCS17@users.noreply.github.com>
2020-08-19 21:01:19 +0100

    Update Third Program.py

---
+++ Python/Third Program.py
@@ -0,0 +1,12 @@
+print("There was a man named George")
+print("he was 70 years old.")
+print("He really liked the name George,")
+print("But he did not like being 70.")
+char_name = "John"
+char_age = 50
+
+print("There was a man named " + char_name + ",")
+print("he was "+ char_age + " years old.")
+char_name = "Mike"
+print("He really liked the name " + char_name + ",")
+print("But he did not like being " + char_age + ".")
