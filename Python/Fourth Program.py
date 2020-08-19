9cb12293813003acb8640d7143d5e5f38ea84ff0
VTCS17 <69059602+VTCS17@users.noreply.github.com>
2020-08-19 21:32:11 +0100

    Create Fourth Program.py

---
+++ Python/Fourth Program.py
@@ -0,0 +1,17 @@
+print("String \n Program")
+print("String \" Program")
+
+phrase = "String Program"
+print(phrase)
+
+print("Concatenation")
+
+print(phrase + " is cool")
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[12])
+print(phrase.index("P"))
+print(phrase.replace("gram","kilogram"))
