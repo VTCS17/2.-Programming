7cd6891acdb551463823a6e78b37b52c347ee46a
VTCS17 <69059602+VTCS17@users.noreply.github.com>
2020-08-26 22:35:46 +0100

    20th Commit

---
+++ Python/19. Guessing Game.py
@@ -0,0 +1,16 @@
+secret_word = "Samu"
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
+while guess != secret_word and not(out_of_guesses):
+    if guess_count < guess_limit:
+        guess = input("Enter the guess:")
+        guess_count += 1
+    else:
+        out_of_guesses = True
+if out_of_guesses:
+    print("You lose!")
+else:
+    print("You won!")
+
