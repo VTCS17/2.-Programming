a5b05cd739980a63456d83d009c037e3eb44f1f0
VTCS17 <69059602+VTCS17@users.noreply.github.com>
2020-08-26 22:51:55 +0100

    21th Commit

---
+++ Python/20. For Loops.py
@@ -0,0 +1,18 @@
+for letter in "Samarth Viswanath":
+    print(letter)
+
+friends = ["Samu","Bunnu", "Savi"]
+for name in friends:
+    print(name)
+
+for index in range(3,10):
+    print(index)
+
+for index in range(len(friends)):
+    print(friends[index])
+
+for index in range(5):
+    if index == 0:
+        print("First Iteration")
+    else:
+        print(index)
