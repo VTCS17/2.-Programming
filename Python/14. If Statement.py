86f01dd0abc954b9dc51badee5137e8a1c3efaa6
VTCS17 <69059602+VTCS17@users.noreply.github.com>
2020-08-26 20:40:41 +0100

    18th Commit

--- Python/14. If Statement.py
+++ Python/14. If Statement.py
@@ -1,10 +1,11 @@
+
 is_male = False
 is_tall = False
 if is_male and is_tall:
     print("You are tall male")
 elif is_male and not(is_tall):
-       print("You are a short male")
+    print("You are a short male")
 elif not(is_male) and is_tall:
-        print("You are not a male but are tall")
+    print("You are not a male but are tall")
 else:
     print("You are either not male or not tall or both")
