d2c358528e09ad0fdde3df18865521baf08a6bc9
VTCS17 <69059602+VTCS17@users.noreply.github.com>
2020-08-27 21:00:50 +0100

    Merge remote-tracking branch 'origin/master'

--- Python/Italianchef.py
+++ Python/Italianchef.py
@@ -1,7 +1,7 @@
 from Chef import chef
-class Italianchef(chef):
+class Italianchef(chef):#Inheriting chef class
 
     def make_pasta(self):
         print("The chef makes pasta")
-    def make_special(self):
+    def make_special(self): #overriding
         print("The chef makes Vangi Bath")
