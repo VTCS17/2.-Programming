37dbc79071ef623fc0c77ec085326008004bdf3f
VTCS17 <69059602+VTCS17@users.noreply.github.com>
2020-08-28 15:17:06 +0100

    1st Commit

---
+++ Python_Practice/Ex 1.1.py
@@ -0,0 +1,5 @@
+print("\t\t\tTo,")
+print("\t\t\tVijayashree Thyagaraja,")
+print("\t\t\tApartment 24, The Bramley,")
+print("\t\t\tOrchard Gardens, Dennehys Cross,")
+print("\t\t\tCork, Ireland - 123456")
